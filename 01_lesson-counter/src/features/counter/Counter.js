63dc5d473a9c2d754f68baab9094f8c70ab9393d
Mohamed Hossam <m.hossameldine@gmail.com>
2023-05-05 16:52:28 +0200

    featue: add counter operations

--- 01_lesson-counter/src/features/counter/Counter.js
+++ 01_lesson-counter/src/features/counter/Counter.js
@@ -1,8 +1,47 @@
+import { useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectCount,
+  increment,
+  decrement,
+  incrementByAmount,
+  decrementByAmount,
+  reset,
+} from "./counterSlice";
+
 const Counter = () => {
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+
+  const inputRef = useRef(null);
+
+  const incrementByAmountHandler = (amount) => {
+    dispatch(incrementByAmount(Number(amount) || 0));
+  };
+
+  const decrementByAmountHandler = (amount) => {
+    dispatch(decrementByAmount(Number(amount) || 0));
+  };
+
+  const resetAllHandler = () => {
+    dispatch(reset());
+    inputRef.current.value = 0;
+  };
+
   return (
     <section>
-      <p></p>
-      <button></button>
+      <p>{count}</p>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <p>Change By Amount</p>
+      <input defaultValue={0} ref={inputRef} type="text" name="" id="" />
+      <button onClick={() => incrementByAmountHandler(inputRef.current.value)}>
+        Increment by amount
+      </button>
+      <button onClick={() => decrementByAmountHandler(inputRef.current.value)}>
+        Increment by amount
+      </button>
+      <button onClick={resetAllHandler}>Reset All</button>
     </section>
   );
 };
