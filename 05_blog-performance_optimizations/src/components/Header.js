e44ab14a67dc7ce290e2b43207052c71589b02b9
Mohamed Hossam <m.hossameldine@gmail.com>
2023-05-13 22:43:45 +0200

    feature: apply memoize selector for user posts

--- 05_blog-performance_optimizations/src/components/Header.js
+++ 05_blog-performance_optimizations/src/components/Header.js
@@ -1,17 +1,30 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { getCount, incrementCount } from "../features/posts/postsSlice";
 
 const Header = () => {
-    return (
-        <header className="Header">
-            <h1>Redux Blog</h1>
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="post">Post</Link></li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
+  const dispatch = useDispatch();
+  const count = useSelector(getCount);
 
-export default Header
+  return (
+    <header className="Header">
+      <h1>Redux Blog</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="post">Post</Link>
+          </li>
+          <li>
+            <Link to="user">Users</Link>
+          </li>
+        </ul>
+        <button onClick={dispatch.bind(null, incrementCount())}>{count}</button>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
