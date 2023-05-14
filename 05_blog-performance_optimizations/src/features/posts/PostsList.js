bf2c207d2b77bd5873979f06c44e165bb0ce1d91
Mohamed Hossam <m.hossameldine@gmail.com>
2023-05-14 23:33:44 +0200

    feature: apply normalized state optimization for posts list

--- 05_blog-performance_optimizations/src/features/posts/PostsList.js
+++ 05_blog-performance_optimizations/src/features/posts/PostsList.js
@@ -1,27 +1,23 @@
 import { useSelector } from "react-redux";
-import { selectAllPosts, getPostsStatus, getPostsError } from "./postsSlice";
+import { selectPostIds, getPostsStatus, getPostsError } from "./postsSlice";
 import PostsExcerpt from "./PostsExcerpt";
 
 const PostsList = () => {
+  const orderedPosts = useSelector(selectPostIds);
+  const postStatus = useSelector(getPostsStatus);
+  const error = useSelector(getPostsError);
 
-    const posts = useSelector(selectAllPosts);
-    const postStatus = useSelector(getPostsStatus);
-    const error = useSelector(getPostsError);
+  let content;
+  if (postStatus === "loading") {
+    content = <p>"Loading..."</p>;
+  } else if (postStatus === "succeeded") {
+    content = orderedPosts.map((postId) => (
+      <PostsExcerpt key={postId} postId={postId} />
+    ));
+  } else if (postStatus === "failed") {
+    content = <p>{error}</p>;
+  }
 
-    let content;
-    if (postStatus === 'loading') {
-        content = <p>"Loading..."</p>;
-    } else if (postStatus === 'succeeded') {
-        const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
-        content = orderedPosts.map(post => <PostsExcerpt key={post.id} post={post} />)
-    } else if (postStatus === 'failed') {
-        content = <p>{error}</p>;
-    }
-
-    return (
-        <section>
-            {content}
-        </section>
-    )
-}
-export default PostsList
+  return <section>{content}</section>;
+};
+export default PostsList;
--- 05_blog-performance_optimizations/src/features/posts/postsSlice.js
+++ 05_blog-performance_optimizations/src/features/posts/postsSlice.js
@@ -3,18 +3,27 @@ import {
   nanoid,
   createAsyncThunk,
   createSelector,
+  createEntityAdapter, // normalized state
 } from "@reduxjs/toolkit";
 import { sub } from "date-fns";
 import axios from "axios";
 
 const POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
 
-const initialState = {
-  posts: [],
+const postsAdapter = createEntityAdapter({
+  sortComparer: (a, b) => b.date.localeCompare(a.date),
+});
+
+/* initial state will be having by default
+ * array of ids
+ * entities object that will be having all the items
+ */
+const initialState = postsAdapter.getInitialState({
+  // extra state that added on top of IDs array and entities object
   status: "idle", //'idle' | 'loading' | 'succeeded' | 'failed'
   error: null,
   count: 0,
-};
+});
 
 export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
   const response = await axios.get(POSTS_URL);
@@ -73,6 +82,7 @@ const postsSlice = createSlice({
     },
   },
   extraReducers(builder) {
+    // postsAdapter has its own CRUD methods
     builder
       .addCase(fetchPosts.pending, (state, action) => {
         state.status = "loading";
@@ -94,7 +104,8 @@ const postsSlice = createSlice({
         });
 
         // Add any fetched posts to the array
-        state.posts = state.posts.concat(loadedPosts);
+        // state.posts = state.posts.concat(loadedPosts);
+        postsAdapter.upsertMany(state, loadedPosts);
       })
       .addCase(fetchPosts.rejected, (state, action) => {
         state.status = "failed";
@@ -123,7 +134,8 @@ const postsSlice = createSlice({
           coffee: 0,
         };
         console.log(action.payload);
-        state.posts.push(action.payload);
+        // state.posts.push(action.payload);
+        postsAdapter.addOne(state, action.payload);
       })
       .addCase(updatePost.fulfilled, (state, action) => {
         if (!action.payload?.id) {
@@ -133,8 +145,9 @@ const postsSlice = createSlice({
         }
         const { id } = action.payload;
         action.payload.date = new Date().toISOString();
-        const posts = state.posts.filter((post) => post.id !== id);
-        state.posts = [...posts, action.payload];
+
+        // state.posts = [...posts, action.payload];
+        postsAdapter.upsertOne(state, action.payload);
       })
       .addCase(deletePost.fulfilled, (state, action) => {
         if (!action.payload?.id) {
@@ -143,19 +156,27 @@ const postsSlice = createSlice({
           return;
         }
         const { id } = action.payload;
-        const posts = state.posts.filter((post) => post.id !== id);
-        state.posts = posts;
+
+        // state.posts = posts;
+        postsAdapter.removeOne(state, id);
       });
   },
 });
 
-export const selectAllPosts = (state) => state.posts.posts;
+// getSelectors creates these selectors and it could be renamed with aliases using destructuring
+export const {
+  selectAll: selectAllPosts,
+  selectById: selectPostById,
+  selectIds: selectPostIds,
+  // pass in a selector that returns the posts slice of state
+} = postsAdapter.getSelectors((state) => state.posts);
+// export const selectAllPosts = (state) => state.posts.posts;
 export const getPostsStatus = (state) => state.posts.status;
 export const getPostsError = (state) => state.posts.error;
 export const getCount = (state) => state.posts.count;
 
-export const selectPostById = (state, postId) =>
-  state.posts.posts.find((post) => post.id === postId);
+// export const selectPostById = (state, postId) =>
+//   state.posts.posts.find((post) => post.id === postId);
 
 // Memoize Selector:
 /*
